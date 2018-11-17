13cd5557e1e3aebb72b147e0b3045513bb43718b
Cornchip <cornchipgonecodin@gmail.com>
2018-11-17 12:40:55 -0500

    Updated item used interface and added test item to test stuff out with

--- Project/core/src/com/tdcrawl/tdc/items/Item.java
+++ Project/core/src/com/tdcrawl/tdc/items/Item.java
@@ -1,6 +1,36 @@
 package com.tdcrawl.tdc.items;
 
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.math.Vector2;
+import com.tdcrawl.tdc.objects.entities.living.LivingEntity;
+
 public abstract class Item
 {
-	// TODO: make
+	/**
+	 * Returns the dimensions for the item when someone will hold them (assuming no scaling)
+	 * @return The dimensions for the item when someone will hold them
+	 */
+	public abstract Vector2 getDimensions();
+	
+	/**
+	 * Called whenever a living entity tries to hold the object
+	 * @return true if they can hold it, false if not
+	 */
+	public boolean onHold(LivingEntity e) { return true; }
+	
+	/**
+	 * Called every time the holder ticks AND is holding the item
+	 * @param delta The time passed since last update
+	 * @param cam The camera used to render where the holder is
+	 */
+	public void tick(float delta, Camera cam) {}
+	
+	public abstract void use(LivingEntity by, UseType type);
+	
+	public enum UseType
+	{
+		LEFT_CLICK,
+		RIGHT_CLICK,
+		MIDDLE_CLICK
+	}
 }
--- Project/core/src/com/tdcrawl/tdc/items/items/TestItem.java
+++ Project/core/src/com/tdcrawl/tdc/items/items/TestItem.java
@@ -1,11 +1,40 @@
 package com.tdcrawl.tdc.items.items;
 
+import com.badlogic.gdx.math.Vector2;
 import com.tdcrawl.tdc.items.Item;
+import com.tdcrawl.tdc.objects.entities.living.LivingEntity;
 import com.tdcrawl.tdc.registries.templates.ItemData;
 import com.tdcrawl.tdc.registries.templates.ItemTemplate;
 
 public class TestItem extends Item
 {
+	@Override
+	public Vector2 getDimensions()
+	{
+		return new Vector2(0.05f, 0.3f);
+	}
+
+	@Override
+	public void use(LivingEntity by, UseType type)
+	{
+		System.out.print("Item Used (");
+		
+		switch(type)
+		{
+			case LEFT_CLICK:
+				System.out.print("left click");
+				break;
+			case RIGHT_CLICK:
+				System.out.print("right click");
+				break;
+			case MIDDLE_CLICK:
+				System.out.print("middle click");
+				break;
+		}
+		
+		System.out.println(")!");
+	}
+	
 	public static class TestItemTemplate implements ItemTemplate
 	{
 		@Override
