2bf28f09ccbe6e97b030dc51d423cd53cedaf95a
Tom Stebbins <tomstebbins@Toms-MacBook-Air.local>
2018-11-22 20:15:16 -0500

    Added to HealingItem

--- Project/core/src/com/tdcrawl/tdc/items/items/healing/HealingItem.java
+++ Project/core/src/com/tdcrawl/tdc/items/items/healing/HealingItem.java
@@ -1,8 +1,25 @@
 package com.tdcrawl.tdc.items.items.healing;
 
 import com.tdcrawl.tdc.items.Item;
+import com.tdcrawl.tdc.objects.entities.living.LivingEntity;
 
 public abstract class HealingItem extends Item
 {
-
+	private int healingValue;
+	private int passiveHeal;
+	
+	@Override
+	/**
+	 * Called whenever the item should be used
+	 * @param by Who it was used by
+	 * @param on Whom it was used on OR the same as by if it is being used on the caster
+	 * @param type The type of use
+	 */
+	public void use(LivingEntity by, LivingEntity on, UseType type)
+	{
+		if(type.equals(UseType.LEFT_CLICK))
+		{
+			by.heal(healingValue);
+		}
+	}
 }
